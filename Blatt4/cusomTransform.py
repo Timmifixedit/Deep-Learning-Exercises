2cb1685cbbcf00c8cb6b3e1f09342910989dac93
Timmifixedit <tim@luchterhand.org>
2020-06-08 17:46:00 +0200

    Paar Kommentare

--- Blatt4/cusomTransform.py
+++ Blatt4/cusomTransform.py
@@ -6,7 +6,8 @@ class MyTransform(object):
         self.abs_angle = rotation_abs_range
 
     def __call__(self, sample):
-        print(type(sample))
+        # When called there seems to be a type mismatch, sample cannot be split up.
+        # The error persists even if I do the transformation directly on the sample.
         image, label = sample
         angle = random.randint(-self.abs_angle, self.abs_angle)
         image.rotate(angle)
